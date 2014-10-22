6b5eb9adb8796b450b433e47cb75fd4839c86f9a
jingwentian <mini.mosquitor@gmail.com>
2014-10-22 11:41:02 +0800

    widget request password needed,output tips

---
+++ template/widget_pass.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>挂件密码验证</title>
+	<link href="http://cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+	<style>
+	#form{
+			max-width: 400px;
+			padding: 15px;
+			margin: 0 auto;
+			padding: 20px;
+			border: 1px solid #eee;
+			border-left-width: 5px;
+			border-radius: 3px;
+		}
+	</style>
+</head>
+<body>
+<div class="container" style="padding:90px 20px;">
+
+    
+    <!--<form id="form"> 
+		  <div class="form-group">
+		    <h4 style="color: #f0ad4e;">访问密码</h4>
+		    <input type="hidden" name="sharelink" id="sharelink" value="fbwx">  
+		    <input type="password" class="form-control" id="password" name="pass" placeholder="Password" required autofocus>
+		  </div>
+		 
+		  <a class="btn btn-default" id="submit">Submit</a>
+	</form>-->
+	<form id="form"> 
+		  <div class="form-group">
+		    <h4 style="color: #f0ad4e;">此文章为加密状态, 暂不支持挂件访问 !</h4>
+		  </div>
+	</form>
+
+
+
+</div>
+
+</body>
+</html>
--- widget.php
+++ widget.php
@@ -15,17 +15,33 @@ use \Michelf\Markdown;
 
 $jing = new Common();
 
-$id = trim($_GET['id'])?:''; //sharelink
-$content = $jing->DB->select("blank",'*', array(
-        "sharelink" => $id,
-        "LIMIT"=>1
-));
+//挂件直接访问
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
 
-$html = Markdown::defaultTransform($content[0]['content']);
-$js_widget .= "document.writeln('<link href=\"http://jingplus.qiniudn.com/static/css/github-markdown-style.css\" media=\"all\" rel=\"stylesheet\" type=\"text/css\" />');"; //挂件携带样式
-$js_widget .= "document.writeln('<div class=\"markdown-body\">".DeleteHtml($html)."</div>');";
+	$id = trim($_GET['id'])?:''; //sharelink
+	$content = $jing->DB->select("blank",'*', array(
+	        "sharelink" => $id,
+	        "LIMIT"=>1
+	));
 
-echo $js_widget;
+	//如果无需密码直接输出内容
+	if($content[0]['password'] == '') {
+		$html = Markdown::defaultTransform($content[0]['content']);
+		$js_widget .= "document.writeln('<link href=\"http://jingplus.qiniudn.com/static/css/github-markdown-style.css\" media=\"all\" rel=\"stylesheet\" type=\"text/css\" />');"; //挂件携带样式
+		$js_widget .= "document.writeln('<div class=\"markdown-body\">".DeleteHtml($html)."</div>');";
+	//如需密码则输出加密提示
+	}else {
+		$js_widget = "document.writeln('".DeleteHtml(file_get_contents('./template/widget_pass.php'))."')";
+	}
+
+	echo $js_widget;
+
+//挂件加密访问
+}elseif($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+}
+
+
 
 /*
 <style> .blank_content{margin:0 auto;padding: 20px;border: 1px solid #eee;border-left-width: 5px;border-radius: 3px;border-left-color: #f0ad4e;}code,pre{font-family: Menlo, Monaco, Consolas, Courier New, monospace;}code {padding: 2px 4px;font-size: 90%;color: #c7254e;background-color: #f9f2f4;border-radius: 4px;}pre {display: block;padding: 9.5px;margin: 0 0 10px;font-size: 13px;line-height: 1.42857143;color: #333;word-break: break-all;word-wrap: break-word;background-color: #f5f5f5;border: 1px solid #ccc;border-radius: 4px;}pre code {padding: 0;font-size: inherit;color: inherit;white-space: pre-wrap;background-color: transparent;border-radius: 0;}</style>
